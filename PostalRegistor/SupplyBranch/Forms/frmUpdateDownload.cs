3dc1d648dba665676cd92aa6666e407ea9706b4b
Shaukatkb <shaukatkb01@gmail.com>
2026-08-15 16:40:15 +0500

    feat(update): add frmUpdateDownload with async progress tracking

---
+++ PostalRegistor/SupplyBranch/Forms/frmUpdateDownload.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SupplyBranch.Forms
+{
+    public partial class frmUpdateDownload : Form
+    {
+        private readonly string _downloadUrl;
+        private readonly string _destinationPath;
+
+        public frmUpdateDownload(string downloadUrl, string destinationPath)
+        {
+            InitializeComponent();
+            _downloadUrl = downloadUrl;
+            _destinationPath = destinationPath;
+        }
+
+        private async void frmUpdateDownload_Load(object sender, EventArgs e)
+        {
+            await StartDownloadAsync();
+        }
+
+        private async Task StartDownloadAsync()
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    using (var response = await client.GetAsync(_downloadUrl, HttpCompletionOption.ResponseHeadersRead))
+                    {
+                        response.EnsureSuccessStatusCode();
+
+                        var totalBytes = response.Content.Headers.ContentLength ?? -1L;
+                        using (var stream = await response.Content.ReadAsStreamAsync())
+                        using (var fileStream = new FileStream(_destinationPath, FileMode.Create, FileAccess.Write, FileShare.None, 8192, true))
+                        {
+                            var buffer = new byte[8192];
+                            long totalRead = 0;
+                            int bytesRead;
+
+                            while ((bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+                            {
+                                await fileStream.WriteAsync(buffer, 0, bytesRead);
+                                totalRead += bytesRead;
+
+                                if (totalBytes != -1)
+                                {
+                                    int progress = (int)((totalRead * 100) / totalBytes);
+                                    progressBar1.Value = progress;
+                                    lblStatus.Text = $"Downloading update... {progress}%";
+                                }
+                            }
+                        }
+                    }
+                }
+
+                this.DialogResult = DialogResult.OK;
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Download failed: " + ex.Message, "Update Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.DialogResult = DialogResult.Cancel;
+                this.Close();
+            }
+        }
+    }
+}
