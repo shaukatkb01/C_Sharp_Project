3aea266834fd3c418bb77e6390b120b2214710b2
Shaukatkb <shaukatkb01@gmail.com>
2026-08-15 13:09:10 +0500

    Add global application version information

---
+++ PostalRegistor/SupplyBranch/Classes/AppVersionInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace SupplyBranch.Classes
+{
+    public static class AppVersionInfo
+    {
+        public static string CurrentVersion { get; set; } = "";
+        public static string AvailableVersion { get; set; } = "";
+    }
+}
